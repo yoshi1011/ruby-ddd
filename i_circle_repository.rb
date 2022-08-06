425d5944aa3d5bf8281ebd15aa52ba59fd07dfe1
Yoshinori Tokuno <yoshi.jpn.10.11@gmail.com>
2022-08-07 00:45:01 +0900

    ICircleRepositoryクラスにメソッド名の重複についてコメント

--- i_circle_repository.rb
+++ i_circle_repository.rb
@@ -5,6 +5,7 @@ class ICircleRepository
     raise NotImplementedError.new('未定義')
   end
 
+  # テキスト通りに書いたが、同じメソッド名が2つできているため修正が必要
   def find(circle_id)
     raise NotImplementedError.new('未定義')
   end
